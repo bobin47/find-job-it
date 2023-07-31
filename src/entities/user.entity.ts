14b218e6676af7bf045a755597444c47a3cfcfac
hoangchuong <lehoangchuong.lk@gmail.com>
2023-08-01 00:18:47 +0700

    init source and module user

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  first_Name: string;
+
+  @Column()
+  last_Name: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column({ default: 1 })
+  status: number;
+}
