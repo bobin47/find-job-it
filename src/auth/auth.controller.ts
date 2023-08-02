0fb6e096ca9b04775815027615c646ac1e67c581
hoangchuong <lehoangchuong.lk@gmail.com>
2023-08-02 10:40:21 +0700

    done auth api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +16,16 @@ export class AuthController {
   @Post('register')
   register(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.register(registerUserDto);
+  }
+
+  @Post('login')
+  @UsePipes(ValidationPipe)
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
+  }
+
+  @Post('refresh_token')
+  refreshToken(@Body() { refresh_token }) {
+    return this.authService.refreshToken(refresh_token);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
 import { RegisterUserDto } from './dto/register-user.dto';
 import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
+    private jwtService: JwtService,
+    private configService: ConfigService,
   ) {}
 
   async register(registerUserDto: RegisterUserDto): Promise<User> {
@@ -21,6 +26,77 @@ export class AuthService {
       refresh_token: 'text',
       password: hashPassword,
     });
+  }
+
+  async login(loginUserDto: LoginUserDto): Promise<any> {
+    const user = await this.userRepository.findOne({
+      where: {
+        email: loginUserDto.email,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException('Email is not exit', HttpStatus.UNAUTHORIZED);
+    }
+
+    const isCheckPassword = bcrypt.compareSync(
+      loginUserDto.password,
+      user.password,
+    );
+
+    if (!isCheckPassword) {
+      throw new HttpException(
+        'Email/password is not correct',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    const payload = { id: user.id, email: user.email };
+
+    return this.generateToken(payload);
+  }
+
+  async refreshToken(refresh_token: string) {
+    try {
+      const verify = await this.jwtService.verifyAsync(refresh_token, {
+        secret: this.configService.get<string>('SECRET'),
+      });
+
+      const CheckExitToken = await this.userRepository.findOneBy({
+        email: verify.email,
+        refresh_token,
+      });
+
+      if (CheckExitToken) {
+        const payload = {
+          id: CheckExitToken.id,
+          email: CheckExitToken.email,
+        };
+
+        return this.generateToken(payload);
+      } else {
+        throw new HttpException(
+          'Refresh token is valid',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+    } catch (error) {
+      throw new HttpException('Refresh token is valid', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async generateToken(payload: { id: number; email: string }) {
+    const access_token = await this.jwtService.signAsync(payload);
+    const refresh_token = await this.jwtService.signAsync(payload, {
+      secret: this.configService.get<string>('SECRET'),
+      expiresIn: this.configService.get<string>('EXP_IN_REFRESH_TOKEN'),
+    });
+
+    await this.userRepository.update(
+      { email: payload.email },
+      { refresh_token },
+    );
+
+    return { access_token, refresh_token };
   }
 
   async handleHashPassword(password: string): Promise<string> {
